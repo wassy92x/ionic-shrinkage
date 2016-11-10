59895fbc8164b03a23988e17048a934df1ac6f48
wassy92x <github@fabian.alias.wassy.de>
2016-11-10 21:46:13 +0100

    moved files to dist

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Shrinkage } from './shrinkage.directive';
-export * from './shrinkage.directive';
-declare var _default: {
-    directives: typeof Shrinkage[];
-};
-export default _default;
