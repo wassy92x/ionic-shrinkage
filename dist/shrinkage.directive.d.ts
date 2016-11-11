5575b00309d037853730ffd25e6756d35cf4c0a5
wassy92x <github@fabian.alias.wassy.de>
2016-11-11 19:40:14 +0100

    files added

---
+++ dist/shrinkage.directive.d.ts
@@ -0,0 +1,33 @@
+import { ElementRef, Renderer, AfterViewInit, OnDestroy, NgZone } from '@angular/core';
+import { Content } from 'ionic-angular';
+export declare class Shrinkage implements AfterViewInit, OnDestroy {
+    private el;
+    private renderer;
+    private zone;
+    private headerHeight;
+    private lastScrollTop;
+    private lastHeaderTop;
+    private showAtTop;
+    private isStatusBarShowing;
+    private pauseForBarAnimation;
+    private pauseForBarDuration;
+    private pauseForBarTimeout;
+    private scrollTop;
+    private contentHeight;
+    private scrollHeight;
+    private scrollChange;
+    private pastBottom;
+    private lastTopFloored;
+    private showParallaxFactor;
+    private hideParallaxFactor;
+    content: Content;
+    showAlways: boolean;
+    constructor(el: ElementRef, renderer: Renderer, zone: NgZone);
+    ngAfterViewInit(): void;
+    ngOnDestroy(): void;
+    resize(): void;
+    render(ts: number): void;
+    readonly showingHeight: number;
+    private onPageScroll(event);
+    calculateRender(timestamp: number): void;
+}
